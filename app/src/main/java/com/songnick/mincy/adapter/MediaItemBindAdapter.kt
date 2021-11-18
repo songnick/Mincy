790f32afedc386869f7ab43f629f08c74b768e93
qfsong <qfsong108@gmail.com>
2021-11-18 22:52:58 +0800

    add permission

--- app/src/main/java/com/songnick/mincy/adapter/MediaItemBindAdapter.kt
+++ app/src/main/java/com/songnick/mincy/adapter/MediaItemBindAdapter.kt
@@ -1,16 +1,18 @@
 package com.songnick.mincy.adapter
 
+import android.net.Uri
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import java.io.File
 
 @BindingAdapter("imageFromUrl")
 fun bindImageUrl(view: ImageView, imageUrl: String?) {
     if (!imageUrl.isNullOrEmpty()){
+        val file = File(imageUrl)
         Glide.with(view.context)
-            .load(imageUrl)
-            .transition(DrawableTransitionOptions.withCrossFade())
+            .load(file)
             .into(view)
     }
 }
